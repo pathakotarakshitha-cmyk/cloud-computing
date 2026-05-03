8237e71f32011e3fcb9641440644f058487b394e
pathakotarakshitha <pathakotarakshitha>
2026-05-03 08:43:17 +0530

    added env file

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const isStudentMode=process.env.REACT_APP_KEY_USE_MODE.toString()==="student"
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{isStudentMode ?"student portal":"Faculty portal"}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
